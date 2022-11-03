8af65a93865963100be5829ea9617b63f14be236
timothy68 <nguyenvannam.marketing@gmail.com>
2022-11-03 14:30:30 +0700

    Update function.php and load css from assets folder

--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@
 
 
 // Includes - Cau lenh
-
+include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/front/head.php'));
 
 // Hooks - Logic xu ly trong theme
+add_action('wp_enqueue_scripts', 'u_enqueue');
+add_action('wp_head', 'u_head', 5);
