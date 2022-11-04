97bd9e05ef677856c6ef906ba69dc11bdf8bc24e
timothy68 <nguyenvannam.marketing@gmail.com>
2022-11-04 17:23:18 +0700

    Update Front End Home - Header

--- functions.php
+++ functions.php
@@ -6,7 +6,10 @@
 // Includes - Cau lenh
 include(get_theme_file_path('/includes/front/enqueue.php'));
 include(get_theme_file_path('/includes/front/head.php'));
+include(get_theme_file_path('/includes/setup.php'));
+
 
 // Hooks - Logic xu ly trong theme
 add_action('wp_enqueue_scripts', 'u_enqueue');
 add_action('wp_head', 'u_head', 5);
+add_action('after_setup_theme', 'u_setup_theme');
